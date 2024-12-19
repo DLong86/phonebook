fd1265b73982fe44a816033cc2716de4f10250c0
DLong <daveallenlong@gmail.com>
2024-12-19 11:28:26 +0900

    added dist/update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(express.static("build"));
+app.use(express.static("dist"));
 
 morgan.token("body", (request) => {
 	if (request.method === "POST") {
